3198c195a7b20c1072c9a9596d5179b54a8a8e4c
=RajuKumarChandra <rajumca.ceg@gmail.com>
2018-09-29 15:10:12 +0530

    First Commit succeded

--- src/helloworldproj/HelloWorldProj.java
+++ src/helloworldproj/HelloWorldProj.java
@@ -17,10 +17,7 @@ public class HelloWorldProj {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World !");
-        System.out.println("Hello World !");
-        System.out.println("Hello World !");
-        System.out.println("Hello World !");
-        System.out.println("Hello World !");
+        
         
         
         
